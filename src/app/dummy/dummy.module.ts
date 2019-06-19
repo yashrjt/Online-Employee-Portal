ffd0b53048419a02306732cc5ac74e122f0c3b00
yash.rjt <esharaagav@outlook.com>
2019-06-19 12:40:38 -0400

    Routing files added!

---
+++ src/app/dummy/dummy.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DummyRoutingModule } from './dummy-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DummyRoutingModule
+  ]
+})
+export class DummyModule { }
