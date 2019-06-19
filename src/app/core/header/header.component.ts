06273adbd05ec527fe3f18c61303717a0a0adf93
yash.rjt <esharaagav@outlook.com>
2019-06-19 11:22:33 -0400

    Third commit!

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  adminLoggedIn:boolean;
   constructor() { }
 
   ngOnInit() {
+    this.adminLoggedIn=false;
   }
 
 }
