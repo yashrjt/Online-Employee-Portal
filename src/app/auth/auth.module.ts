041fe3aecf05cbd8a077f9ecee706a2b136b5fd6
Guangheng Tong <38570533+Ghtong@users.noreply.github.com>
2019-06-19 10:29:55 -0500

    add employee login and admin login

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SignupComponent } from './signup/signup.component';
+import { EmployeeLoginComponent } from './employee-login/employee-login.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
 
 @NgModule({
-  declarations: [ SignupComponent],
+  declarations: [ SignupComponent, EmployeeLoginComponent, AdminLoginComponent],
   imports: [
     CommonModule
   ]
