ecc0eaa6b562993fb43d9b2bc885a320bf12080f
lw2533315 <wl36901@gmail.com>
2019-06-28 20:07:28 -0700

    updata

--- src/app/auth/admin-login/admin-login.component.ts
+++ src/app/auth/admin-login/admin-login.component.ts
@@ -7,16 +7,11 @@ import { LoginInfo } from '../../auth/login-info'
   styleUrls: ['./admin-login.component.css']
 })
 export class AdminLoginComponent implements OnInit {
-<<<<<<< HEAD
 
   private logginInfo: LoginInfo;
 
 
 
-=======
-  
-  constructor() { }
->>>>>>> 15a417de4514ed782bf614fefefabf89da289687
 
   ngOnInit() {
   }
